35e39a4cdeef399078552f24d50dd7226fdf68f6
muokicaleb <muokicaleb@gmail.com>
2019-01-19 21:27:25 +0300

    yet to fix error

--- weather_app/app.py
+++ weather_app/app.py
@@ -9,13 +9,19 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
 
 db = SQLAlchemy(app)
 
+
 class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
 
+
+global new_city
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+
         new_city = request.form.get('city')
     if new_city:
         new_city_obj = City(name=new_city)
@@ -23,20 +29,18 @@ def index():
         db.session.commit()
 
     cities = City.query.all()
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid='+API_KEY
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=' + API_KEY
     weather_data = []
     for city in cities:
         r = requests.get(url.format(city.name)).json()
         weather = {
-                'city': city.name,
-                'temperature': r['main']['temp'],
-                'description': r['weather'][0]['description'],
-                'icon': r['weather'][0]['icon'],}
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'], }
         weather_data.append(weather)
 
-
     return render_template('index.html', weather_data=weather_data)
-
 
 
 if __name__ == '__main__':
