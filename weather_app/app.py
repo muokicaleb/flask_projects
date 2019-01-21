0104fb951621fe74dc72f456f8dd5d9200c16167
muokicaleb <muokicaleb@gmail.com>
2019-01-21 11:48:16 +0300

    corrected bug

--- weather_app/app.py
+++ weather_app/app.py
@@ -15,15 +15,11 @@ class City(db.Model):
     name = db.Column(db.String(50), nullable=False)
 
 
-global new_city
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        global new_city
         new_city = request.form.get('city')
-    if new_city:
+    #if new_city:
         new_city_obj = City(name=new_city)
         db.session.add(new_city_obj)
         db.session.commit()
