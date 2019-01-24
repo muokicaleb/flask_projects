46c0ac6b3c32932ef29c8603a2be0ccafbd4c0e5
muokicaleb <muokicaleb@gmail.com>
2019-01-24 18:32:22 +0300

    removing db

--- weather_app/app.py
+++ weather_app/app.py
@@ -1,10 +1,10 @@
 import requests
 from cred import API_KEY
-from flask import Flask
-from flask import render_template, request
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
 
 db = SQLAlchemy(app)
@@ -19,21 +19,30 @@ class City(db.Model):
 def index():
     if request.method == 'POST':
         new_city = request.form.get('city')
-    #if new_city:
-        new_city_obj = City(name=new_city)
-        db.session.add(new_city_obj)
-        db.session.commit()
+
+        if new_city:
+            new_city_obj = City(name=new_city)
+
+            db.session.add(new_city_obj)
+            db.session.commit()
 
     cities = City.query.all()
+
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=' + API_KEY
+
     weather_data = []
+
     for city in cities:
-        r = requests.get(url.format(city.name)).json()
+        cname = 'Nairobi'
+        r = requests.get(url.format('Mombasa')).json()
+
         weather = {
             'city': city.name,
             'temperature': r['main']['temp'],
             'description': r['weather'][0]['description'],
-            'icon': r['weather'][0]['icon'], }
+            'icon': r['weather'][0]['icon'],
+        }
+
         weather_data.append(weather)
 
     return render_template('index.html', weather_data=weather_data)
