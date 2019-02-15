62d7208094d96187e2db51f59829e6ef0bdae3cb
muokicaleb <muokicaleb@gmail.com>
2019-02-15 11:29:54 +0300

    working on app.y for functionality

--- regestration_form/app.py
+++ regestration_form/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import url_for
+from form import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, login_user, logout_user, current_user
@@ -10,4 +11,13 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def reg():
-    render_template('index.html')
+    return render_template('index.html')
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
