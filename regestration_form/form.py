513c9bbedeb25faa96ef05cbe35b116af4f09c94
muokicaleb <muokicaleb@gmail.com>
2019-02-16 17:28:00 +0300

    done

--- regestration_form/form.py
+++ regestration_form/form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
@@ -7,15 +7,17 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=1, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
+
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
 
-    submit_signup = SubmitField('Sign Up')
+    submit = SubmitField('Sign Up')
+   # phone = IntegerField('Phone', validators=[DataRequired(), Length(min=10, max=16)])
 
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
-    submit_login = SubmitField('Login')
+    submit = SubmitField('Login')
