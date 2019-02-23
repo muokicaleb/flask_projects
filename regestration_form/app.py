5e824bf30ce31a7fa77fde1409686124bf5c1602
muokicaleb <muokicaleb@gmail.com>
2019-02-23 08:14:29 +0300

    Revert "done"
    This reverts commit 513c9bbedeb25faa96ef05cbe35b116af4f09c94.

--- regestration_form/app.py
+++ regestration_form/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template, redirect
+from flask import render_template
 from flask import url_for
 from form import RegistrationForm, LoginForm
 from flask_sqlalchemy import SQLAlchemy
@@ -7,63 +7,16 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, login_user, logout_user, current_user
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '315742e65f9228cea7c3d52418hja5e3'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-db = SQLAlchemy(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-
-
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True)
-    email = db.Column(db.String(80), unique=True)
-    password = db.Column(db.String(50))
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 
 @app.route("/", methods=['GET', 'POST'])
 def reg():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
-        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-
-        return redirect(url_for('login'))
-    return render_template('index.html', form=form)
+    return render_template('index.html')
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user:
-            if check_password_hash(user.password, form.password.data):
-                login_user(user, remember=form.remember.data)
-                return redirect(url_for('home'))
-        return '<h1> Invalid  password</h1>'
-    return render_template('login.html', form=form)
-
-
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
-
-
-@app.route("/home")
-@login_required
-def home():
-    return render_template('home.html')
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
--- regestration_form/form.py
+++ regestration_form/form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
@@ -7,17 +7,15 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=1, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
 
-    submit = SubmitField('Sign Up')
-   # phone = IntegerField('Phone', validators=[DataRequired(), Length(min=10, max=16)])
+    submit_signup = SubmitField('Sign Up')
 
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+    submit_login = SubmitField('Login')
