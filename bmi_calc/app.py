386bb645206a09aba3c755e8d8fff25f32162d3d
muokicaleb <muokicaleb@gmail.com>
2019-02-25 18:20:53 +0300

    change

--- bmi_calc/app.py
+++ bmi_calc/app.py
@@ -1,23 +1,23 @@
 from flask import Flask
-from flask import redirect
 from flask import render_template
 from flask import request
 from forms import input
 from bmi_calc import calc
 
 app = Flask(__name__)
-
-bmi = ''
+app.config['SECRET_KEY'] = '315742e65f9228cea7c3d52418hja5e3'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    bmi = ''
     form = input(request.form)
     if request.method == 'POST' and form.validate():
-        kg = form.weight.data
-        cm = form.height.data
+        kg = float(request.form.get('weight'))
+        cm = float(request.form.get('height'))
         bmi = calc(kg, cm)
-    render_template('index.html', form=form, bmi=bmi)
+    return render_template('index.html', form=form, bmi=bmi)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- bmi_calc/forms.py
+++ bmi_calc/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired
-from wtforms import StringField, IntegerField, BooleanField, SubmitField
+from wtforms import IntegerField, SubmitField
 
 
 class input(FlaskForm):
-    Weight = IntegerField('Weight in kg', validators=[DataRequired()])
+    weight = IntegerField('Weight in kg', validators=[DataRequired()])
     height = IntegerField('Height in cm', validators=[DataRequired()])
     submit = SubmitField('Calculate')
