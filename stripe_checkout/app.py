dc2dfd5c9ff1bf605ecc7c64eac792691212a5e9
muokicaleb <muokicaleb@gmail.com>
2019-04-23 10:30:20 +0100

    imported stripe keys from cred file as os brought errors which will be addressed on 25th

--- stripe_checkout/app.py
+++ stripe_checkout/app.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from flask import render_template
+from creds import STRIPE_SECRET_KEY, STRIPE_PUBLISHABLE_KEY
+import stripe
+
 
 app = Flask(__name__)
+stripe_keys = {
+    'secret_key': STRIPE_SECRET_KEY,
+    'publishable_key': STRIPE_PUBLISHABLE_KEY
+}
+
+
+stripe.api_key = stripe_keys['secret_key']
 
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', key=stripe_keys['publishable_key'])
 
 
 if __name__ == "__main__":
