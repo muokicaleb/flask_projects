2c5b676bd91877223cd79595bea534aa8f5029a5
muokicaleb <muokicaleb@gmail.com>
2019-05-08 14:44:20 +0300

    Added construction route

--- digital_drug/app.py
+++ digital_drug/app.py
@@ -9,5 +9,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/construction')
+def construction():
+    return render_template('construction.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
