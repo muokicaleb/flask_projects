a5dfa3fddf27f3a3eb498e535542a750e12eb303
muokicaleb <muokicaleb@gmail.com>
2019-05-18 11:15:13 +0100

    twitter graph  ^bd

---
+++ tweetergraph/cred.py
@@ -0,0 +1,5 @@
+# user credentials
+access_token = "1012740340871254019-KFmKWZaAJvYxe992M7aa0nLbbdzn28"
+access_secret = "6flSRli7d9eqC2NDHUzcAGfmo5qlxsftLpegt47ncBWvG"
+consumer_key = "HA40PiOT3SeZtTEOBuE3aEJgw"
+consumer_secret = "DrBeQogUDPPMsWrLAzCNZ1pu2Xt6tdUeqr5Dkiw20hzxfeLbNO"
